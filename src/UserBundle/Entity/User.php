069b9dbb13fe9894eb5531dff8ee4c25f5dcce71
margothsc <m.hanszechuen@gmail.com>
2016-07-04 15:18:36 +0200

    become premium in navbar

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -657,4 +657,28 @@ class User extends BaseUser
     {
         return $this->pictureName;
     }
+
+    /**
+     * Set status
+     *
+     * @param string $status
+     *
+     * @return User
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
 }
