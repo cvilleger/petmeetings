835e67c835eafb9d9096a113cec4c15327a10a79
Your Name <you@example.com>
2016-07-09 19:58:17 +0200

    Révision de la home

--- src/UserBundle/Controller/ProfileController.php
+++ src/UserBundle/Controller/ProfileController.php
@@ -14,68 +14,68 @@ use AppBundle\Service\UploadService;
 
 class ProfileController extends BaseController
 {
-    /** @var UploadService uploadService */
-    private $uploadService;
-    /** @var  Request $request */
-    private $request;
+	/** @var UploadService uploadService */
+	private $uploadService;
+	/** @var  Request $request */
+	private $request;
 
-    /**
-     * @param Request $request
-     */
-    public function preExecute(Request $request){
-        $this->uploadService = $this->container->get('UploadService');
-        $this->request = $request;
-    }
+	/**
+	 * @param Request $request
+	 */
+	public function preExecute(Request $request){
+		$this->uploadService = $this->container->get('UploadService');
+		$this->request = $request;
+	}
 
-    public function findAction(User $user)
-    {
-        $locale = 'fr';
+	public function findAction(User $user)
+	{
+		$locale = 'fr';
 
-        return $this->render('UserBundle:Profile:profile.html.twig', array(
-            'user' => $user,
-            'locale' => $locale
-        ));
-    }
+		return $this->render('UserBundle:Profile:profile.html.twig', array(
+			'user' => $user,
+			'locale' => $locale
+			));
+	}
 
-    public function showAction()
-    {
-        $user = $this->getUser();
-        $locale = 'fr';
+	public function showAction()
+	{
+		$user = $this->getUser();
+		$locale = 'fr';
 
-        return $this->render('UserBundle:Profile:show.html.twig', array(
-            'user' => $user,
-            'locale' => $locale
-        ));
-    }
+		return $this->render('UserBundle:Profile:show.html.twig', array(
+			'user' => $user,
+			'locale' => $locale
+			));
+	}
 
-    public function editAction(Request $request)
-    {
-        $user = $this->getUser();
-        $form = $this->createForm(UserEditType::class, $user);
+	public function editAction(Request $request)
+	{
+		$user = $this->getUser();
+		$form = $this->createForm(UserEditType::class, $user);
 
-        $form->handleRequest($request);
-        if($form->isValid()) {
-            $file = $form->get('picture')->getData();
-            $fileAnimal = $form->get('animal')->get('picture')->getData();
+		$form->handleRequest($request);
+		if($form->isValid()) {
+			$file = $form->get('picture')->getData();
+			$fileAnimal = $form->get('animal')->get('picture')->getData();
 
-            if(!empty($file)) {
-                $uniqueFileName = $this->uploadService->uploadFile($file);
-                $user->setPictureName($uniqueFileName);
-            }
-            if(!empty($fileAnimal)) {
-                $uniqueFileNameAnimal = $this->uploadService->uploadFile($file);
-                $user->getAnimal()->setPictureName($uniqueFileNameAnimal);
-            }
-            
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
+			if(!empty($file)) {
+				$uniqueFileName = $this->uploadService->uploadFile($file);
+				$user->setPictureName($uniqueFileName);
+			}
+			if(!empty($fileAnimal)) {
+				$uniqueFileNameAnimal = $this->uploadService->uploadFile($file);
+				$user->getAnimal()->setPictureName($uniqueFileNameAnimal);
+			}
 
-            return $this->redirect($this->generateUrl('fos_user_profile_show'));
-        }
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($user);
+			$em->flush();
 
-        return $this->render('UserBundle:Profile:edit.html.twig', array(
-            'form' => $form->createView()
-        ));
-    }
+			return $this->redirect($this->generateUrl('fos_user_profile_show'));
+		}
+
+		return $this->render('UserBundle:Profile:edit.html.twig', array(
+			'form' => $form->createView()
+			));
+	}
 }
